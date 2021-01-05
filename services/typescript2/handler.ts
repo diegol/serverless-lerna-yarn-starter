d715a5e47a847434ce7e73877103a4da326b4a8e
Diegol <diego.lewin@gmail.com>
2021-01-05 14:48:57 +1300

    adding nestjs

--- services/typescript2/handler.ts
+++ services/typescript2/handler.ts
@@ -1,9 +1,20 @@
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { NestFactory } from '@nestjs/core';
 
-export async function hello(
-  event: APIGatewayEvent,
-  context: Context
-): Promise<APIGatewayProxyResult> {
+import { AppModule } from './app.module';
+import { AppService } from './app.service';
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  return app;
+}
+
+
+export async function hello(event, context) {
+  const app = await bootstrap();
+  const appService = app.get(AppService);
+  await appService.getHello();
+
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -13,3 +24,23 @@ export async function hello(
     }),
   };
 }
+
+/*
+export async function hello(
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+
+
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v2.0! Your function executed successfully! type",
+      context,
+      event,
+    }),
+  };
+}
+*/
